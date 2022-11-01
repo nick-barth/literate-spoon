06032d4a624f994b283eab387a4e9396f57ae2ce
nick-barth <barth.nicholas@gmail.com>
2022-11-01 14:43:27 +0100

    only scales when greater than height

--- script.js
+++ script.js
@@ -17,6 +17,7 @@ window.onload = function (e) {
 
   const imageUrls = [
     "https://images.unsplash.com/photo-1666085575722-bdf8a510c2c1?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1011&q=80",
+    "http://clipart-library.com/data_images/320465.png",
     "https://images.unsplash.com/photo-1607788156334-50076e4c7624?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=2070&q=80",
     "https://images.unsplash.com/photo-1666786881194-7ecf6f620375?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1064&q=80",
     "https://images.unsplash.com/photo-1535136072409-ff0c7a947733?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=2049&q=80",
@@ -58,17 +59,22 @@ function renderImages() {
   canvas.width = images.length * FRAME_WIDTH;
   for (let i = 0; i < images.length; i++) {
     const image = images[i];
-    const ratio = image.width / image.height;
-    let newWidth = canvas.width;
-    let newHeight = newWidth / ratio;
-    if (newHeight > canvas.height) {
-      newHeight = canvas.height;
-      newWidth = newHeight * ratio;
-    }
-    const xOffset = FRAME_WIDTH * i + (FRAME_WIDTH - newWidth) / 2;
-    const yOffset =
-      newHeight < canvas.height ? (canvas.height - newHeight) / 2 : 0;
 
-    ctx.drawImage(image, xOffset, yOffset, newWidth, newHeight);
+    // Only scale if height is greater than canvas
+    if (image.height > canvas.height) {
+      const scale = Math.min(
+        FRAME_WIDTH / image.width,
+        canvas.height / image.height
+      );
+      const w = image.width * scale;
+      const h = image.height * scale;
+      const left = FRAME_WIDTH / 2 - w / 2 + i * FRAME_WIDTH;
+      const top = canvas.height / 2 - h / 2;
+      ctx.drawImage(image, left, top, w, h);
+    } else {
+      const left = FRAME_WIDTH / 2 - image.width / 2 + i * FRAME_WIDTH;
+      const top = canvas.height / 2 - image.height / 2;
+      ctx.drawImage(image, left, top, image.width, image.height);
+    }
   }
 }
